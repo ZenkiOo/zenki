213d941c3e35a960d7d8805a8f9e3517bd2a42f9
ZenkiOo <zenky@list.ru>
2020-07-14 17:38:52 +0300

    add script for animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,24 @@
-let scene = document.getElementById("scene");
-let parallaxInstance = new Parallax(scene);
+let scene = document.getElementById("scene")
+let parallaxInstance = new Parallax(scene)
+
+let selectedSpan;
+let anim = document.getElementById("row")
+let anim2 = document.getElementById("row2")
+let anim3 = document.getElementById("row3")
+let animRubberBand = function(event) {
+  let target = event.target
+  if (target.tagName != 'SPAN') return
+  animate(target)
+  setTimeout(() => {reanimate(target)}, 1000)
+}
+function animate(span) {
+  selectedSpan = span
+  selectedSpan.classList.add('letter-animated')
+}
+function reanimate(span) {
+  selectedSpan = span
+  selectedSpan.classList.remove('letter-animated')
+}
+anim.onmouseover = animRubberBand
+anim2.onmouseover = animRubberBand
+anim3.onmouseover = animRubberBand
